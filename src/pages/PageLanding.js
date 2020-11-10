4483338f76db1edd6b29244d720df7cdad2fc724
Ashley Zhuang <ashleyzhuang01@gmail.com>
2020-11-09 21:48:46 -0800

    Filter by difficulty

--- src/pages/PageLanding.js
+++ src/pages/PageLanding.js
@@ -12,38 +12,43 @@ class PageLanding extends React.Component {
       this.state = {titles: '', loaded: false};
     }
 
-
-    componentDidUpdate(prevProps, prevState) {
-      if(!this.state.loaded){
+    componentDidMount() {
+      if (!this.state.loaded) {
         const results = this.props.firebase.database().ref('questions').on('value', data => {
-        if(data.val() !== null) {
+        if (data.val() !== null) {
           let obj = data.val();
-          let titles = Object.keys(obj).map(key => {
+          let questions = Object.keys(obj).map(key => {
             return {
-              title: obj[key].title
+              title: obj[key].title,
+              difficulty: obj[key].difficulty,
+              topics: obj[key].topics,
             }
           });
-          this.setState({titles, loaded: true});
+          this.setState({questions, loaded: true});
          }
        });
       }
-      console.log('hi');
+    }
+
+    handleChange = diff => {
+      this.setState({sortBy: diff});
     }
 
     render() {
-      if (!isLoaded(this.props.questions) || !this.state.loaded) {
+      if (!this.state.loaded) {
+        console.log(this.state)
         return (<div >Loading...</div>);
       }
 
-      var db = this.props.firebase.database();
-    var ref = db.ref("questions");
-    ref.on("value", function(snapshot) {
-      console.log(Object.keys(snapshot.val()));
-      var bop = Object.keys(snapshot.val())}, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-      })
+      // var db = this.props.firebase.database();
+      // var ref = db.ref("questions");
+      // ref.on("value", function(snapshot) {
+      //   console.log(Object.keys(snapshot.val()));
+      //   var bop = Object.keys(snapshot.val())}, function (errorObject) {
+      //   console.log("The read failed: " + errorObject.code);
+      // });
 
-      console.log(this.state.titles);
+      console.log(this.state.questions);
 
 
       const easy = (
@@ -69,38 +74,40 @@ class PageLanding extends React.Component {
       );
 
       const quests =
-        Object.keys(this.props.questions).map(questId => {
-          const quest = this.props.questions[questId];
+        Object.keys(this.state.questions)
+          .filter(questId => !this.state.sortBy || this.state.questions[questId].difficulty == this.state.sortBy)
+          .map(questId => {
+            const quest = this.state.questions[questId];
 
-          const topics = quest.topics &&
-            Object.keys(quest.topics).map(topicId => {
-              return (
-                <span className='topic' key={topicId}>{quest.topics[topicId]} </span>
-              );
+            const topics = quest.topics &&
+              Object.keys(quest.topics).map(topicId => {
+                return (
+                  <span className='topic' key={topicId}>{quest.topics[topicId]} </span>
+                );
+            });
+
+            let bars;
+            if(quest.difficulty && quest.difficulty === 'easy'){
+              bars = easy;
+            }
+            if(quest.difficulty && quest.difficulty === 'medium'){
+              bars = medium;
+            }
+            if(quest.difficulty && quest.difficulty === 'hard'){
+              bars = hard;
+            }
+
+            return (
+              <div className='question' key={questId}>
+                <div><Link className='question-title' to={`/q/${questId}`}>#{questId}: {quest.title}</Link></div>
+                <br />
+                <div className='topics'>&nbsp;&nbsp;{topics}</div>
+                <div>{bars}</div>
+                <div ></div>
+                <br />
+              </div>
+            );
           });
-
-          let bars;
-          if(quest.difficulty && quest.difficulty === 'easy'){
-            bars = easy;
-          }
-          if(quest.difficulty && quest.difficulty === 'medium'){
-            bars = medium;
-          }
-          if(quest.difficulty && quest.difficulty === 'hard'){
-            bars = hard;
-          }
-
-          return (
-            <div className='question' key={questId}>
-              <div><Link className='question-title' to={`/q/${questId}`}>#{questId}: {quest.title}</Link></div>
-              <br />
-              <div className='topics'>&nbsp;&nbsp;{topics}</div>
-              <div>{bars}</div>
-              <div ></div>
-              <br />
-            </div>
-          );
-        });
         //<div>&nbsp;&nbsp;Difficulty: {quest.difficulty}</div>
       return (
         <div className='background'>
@@ -109,13 +116,16 @@ class PageLanding extends React.Component {
               <div className='info'>Hireglyph is the future of collaborative interview preparation. Using Hireglyph, you can view other users’ thought processes (TPs) so that you can understand the correct logic behind solving complicated interview problems. Additionally, you can receive feedback on your own TPs from college students and financial professionals alike.</div>
               <div className='info-email'>Questions, comments, or concerns? Email us at <a className='email-link' href = "mailto: admin@hireglyph.com">admin@hireglyph.com</a>.</div>
               <br />
-              <div> {this.state.titles[0].title}</div>
           </div>
           <br />
           <br />
           <div className='questions'>
-            <h1 className='header'>Quantitative Analysis Questions</h1>
-            <div>{quests}</div>
+          <h1 className='header'>Quantitative Analysis Questions</h1>
+          <button onClick={() => this.handleChange('')}>Original list</button>
+          <button onClick={() => this.handleChange('easy')}>Filter by easy</button>
+          <button onClick={() => this.handleChange('medium')}>Filter by medium</button>
+          <button onClick={() => this.handleChange('hard')}>Filter by hard</button>
+          <div>{quests}</div>
 
           </div>
         </div>
@@ -125,13 +135,12 @@ class PageLanding extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    questions: state.firebase.data.questions,
     email: state.firebase.auth.email,
     uid: state.firebase.auth.uid,
   };
 }
 
 export default compose(
-  firebaseConnect(['/questions']),
+  firebaseConnect(),
   connect(mapStateToProps)
 )(PageLanding);
