65be42231a8b8a7c3ccc8449c1a2b97dbc48a535
physicsballer067 <shethhriday29@gmail.com>
2020-11-17 23:56:48 -0800

    app.js updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PageLanding from './pages/PageLanding';
 import PageProfile from './pages/PageProfile';
 import PageQuestion from './pages/PageQuestion';
+import PageEasyQuestion from './pages/PageEasyQuestion';
 import PageTp from './pages/PageTp';
+import PageSa from './pages/PageSa';
 import PageRegister from './pages/PageRegister';
 import PageLogin from './pages/PageLogin';
 import NavBar from './components/NavBar';
@@ -24,8 +26,14 @@ function App() {
         <Route exact path="/q/:questId">
           <PageQuestion />
         </Route>
+        <Route exact path="/eq/:questId">
+          <PageEasyQuestion />
+        </Route>
         <Route exact path="/tp/:questId/:tpId">
           <PageTp />
+        </Route>
+        <Route exact path="/sa/:questId/:saId">
+          <PageSa />
         </Route>
         <Route exact path="/register">
           <PageRegister />
