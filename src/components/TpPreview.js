6d11e3f9d7d0475411729e08c1dd5a68129bec69
shethhriday29 <shethhriday29@gmail.com>
2021-01-18 22:12:26 -0800

    wonky indentation

--- src/components/TpPreview.js
+++ src/components/TpPreview.js
@@ -5,36 +5,36 @@ import { length } from '../constants/PrevLength';
 
 export default class TpPreview extends Component {
   constructor(props) {
-  	super(props);
+    super(props);
   }
   
   render() {
-  	const { initial, approach, solution, expanded } = this.props;
-	  return(
-	  	<div>
-	  		<link
-	      href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
-	      rel="stylesheet"
-	      />
-	      <div>
-					<span className='tp-preview-head' >{initial ? 'Initial:' : ''}</span>
-					<span className='tp-preview-tail'> 
-						{expanded ? <Latex>{initial}</Latex> : initial && <Latex>{initial.slice(0,length+1) + (initial.length > length ? '...' : '')}</Latex>}
-	      	</span>
-				</div>
-	      <div>
-					<span className='tp-preview-head' >{approach ? 'Approaches:' : ''}</span>
-					<span className='tp-preview-tail'>  
-						{expanded ? <Latex>{approach}</Latex> : approach && <Latex>{approach.slice(0,length+1) + (approach.length > length ? '...' : '')}</Latex>}
-	      	</span>
-				</div>
-				<div>
-					<span className='tp-preview-head' >{solution ? 'Solution:' : ''}</span>
-					<span className='tp-preview-tail'> 
-						{expanded ? <Latex>{solution}</Latex> : solution && <Latex>{solution.slice(0,length+1) + (solution.length > length ? '...' : '')}</Latex>}
-	      	</span>
-				</div>
-	    </div>
-	  );
+    const { initial, approach, solution, expanded } = this.props;
+    return(
+      <div>
+        <link
+        href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
+        rel="stylesheet"
+        />
+        <div>
+          <span className='tp-preview-head' >{initial ? 'Initial:' : ''}</span>
+          <span className='tp-preview-tail'> 
+            {expanded ? <Latex>{initial}</Latex> : initial && <Latex>{initial.slice(0,length+1) + (initial.length > length ? '...' : '')}</Latex>}
+          </span>
+        </div>
+        <div>
+          <span className='tp-preview-head' >{approach ? 'Approaches:' : ''}</span>
+          <span className='tp-preview-tail'>  
+            {expanded ? <Latex>{approach}</Latex> : approach && <Latex>{approach.slice(0,length+1) + (approach.length > length ? '...' : '')}</Latex>}
+          </span>
+        </div>
+        <div>
+          <span className='tp-preview-head' >{solution ? 'Solution:' : ''}</span>
+          <span className='tp-preview-tail'> 
+            {expanded ? <Latex>{solution}</Latex> : solution && <Latex>{solution.slice(0,length+1) + (solution.length > length ? '...' : '')}</Latex>}
+          </span>
+        </div>
+      </div>
+    );
   }
 }
