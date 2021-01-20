3c33072fc92b3ff9e75a2cfd3342fa9ddd82bf91
shethhriday29 <shethhriday29@gmail.com>
2021-01-19 23:32:32 -0800

    made loading a functional component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactLoading from 'react-loading';
 
-export default class Loading extends Component {
-    constructor(props) {
-      super(props);
-    }
-
-    render() {
-      return (<ReactLoading className="loading" type={"bars"} width={200}/>);
-    }
+var Loading = function() {
+  return (<ReactLoading className="loading" type={"bars"} width={200}/>);
 }
+
+export default Loading;
