4e6990fefb6030d39e8c1452cc79da26f8598dc0
physicsballer067 <shethhriday29@gmail.com>
2021-01-22 23:33:19 -0800

    made abstraction

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,55 +13,64 @@ class NavBar extends React.Component {
 
   handleClick = link => () => this.props.history.push(link);
 
-  render() {
-    if (!this.props.isLoaded || !isLoaded(this.props.hasNotifs)) {
-      return (
-        <div className='navbar'>
-          <div className='title'> 
-            <Link className='title-text' to='/'>Hireglyph</Link>
-          </div>
-        </div>
-      );
+  navbarContent = () => {
+    if (!this.props.isLoaded || !isLoaded(this.props.hasNotifs)) {  
+      return;
     }
-    
     if (!this.props.uid) {
       return (
-        <div className='navbar'>
-          <div className='title'>
-            <Link className='title-text' to='/'>Hireglyph</Link> 
-          </div>
+        <div>
           <div className='link-click'>
             <Link className='link-text' to='/register'>Register</Link>
           </div>
           <div className='button-click'> 
-            <button className='button-text' onClick={this.handleClick('/login')}>Login</button>
+            <button 
+            className='button-text' 
+            onClick={this.handleClick('/login')}>
+              Login
+            </button>
           </div>
         </div>
       );
     }
-
     return (
-      <div className='navbar'>
-        <div className='title'>
-          <Link className='title-text' to='/'>Hireglyph</Link>
-        </div>
+      <div>
         <div className='link-click2'> 
-          <Link className={!this.props.hasNotifs ? 'link-text' : 'link-text2'} to='/notifications'>Notifications</Link>
+          <Link 
+          className={!this.props.hasNotifs ? 'link-text' : 'link-text2'} 
+          to='/notifications'>
+            Notifications
+          </Link>
         </div>
         <div className='link-click'>
           <Link className='link-text' to='/profile/tp'>Profile</Link>
         </div>
         <div className='button-click'>
-          <button className='button-text' onClick={() => {this.props.firebase.logout();window.location.href="/"}} >Logout</button>
+          <button 
+          className='button-text' 
+          onClick={() => {this.props.firebase.logout();window.location.href="/"}} >
+            Logout
+          </button>
         </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className='navbar'>
+        <div className='title'>
+          <Link className='title-text' to='/'>Hireglyph</Link>
+        </div>
+        {this.navbarContent()}
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, props) => {
   return {
-    hasNotifs: state.firebase.data.hasNotifs,
+    hasNotifs: state.firebase.data.hasNotifs && state.firebase.data.hasNotifs[props.uid],
   };
 }
 
@@ -70,7 +79,6 @@ export default compose(
   firebaseConnect(props => [
     {
       path: '/hasNotifs/' + props.uid,
-      storeAs: 'hasNotifs'
     },
   ]),
   connect(mapStateToProps)
