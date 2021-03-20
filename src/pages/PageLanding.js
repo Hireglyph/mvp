72ed7794267e2f0396b61c0aa1cd057f2df69e01
Ashley Zhuang <ashleyzhuang01@gmail.com>
2021-03-20 12:41:22 -0700

    Finish landing page

--- src/pages/PageLanding.js
+++ src/pages/PageLanding.js
@@ -2,7 +2,13 @@
 
 import { Link } from 'react-router-dom';
 import { jsx } from 'theme-ui'
+
 import meeting from 'assets/images/meeting.svg';
+import submitTPs from 'assets/images/submitTPs.png';
+import postFeedbacks from 'assets/images/postFeedbacks.png';
+import communicate from 'assets/images/communicate.svg';
+import lightbulb from 'assets/images/lightbulb.svg';
+import network from 'assets/images/network.svg';
 
 const PageLandingSx = {
   display: 'flex',
@@ -12,11 +18,10 @@ const PageLandingSx = {
   color: 'white',
   paddingTop: '60px',
   paddingBottom: '20px',
-  paddingLeft: '180px',
 
-  '.top-elements': {
+  '.flex-elements': {
     display: 'flex',
-    flexDirection: 'row',
+    marginTop: '40px',
   },
 
   '.main-text': {
@@ -26,7 +31,7 @@ const PageLandingSx = {
   '.heading': {
     fontWeight: '600',
     fontSize: '45px',
-    letterSpacing: '2px'
+    letterSpacing: '1.5px'
   },
 
   '.description': {
@@ -55,13 +60,37 @@ const PageLandingSx = {
     '&:hover': {
       backgroundColor: 'darkOrange',
     }
-  }
+  },
+
+  '.body-text': {
+    width: '300px',
+  },
+
+  '.icon-caption': {
+    marginTop: '8px',
+    width: '200px',
+  },
+
+  '.icon-container': {
+    textAlign: 'center',
+    marginRight: '110px',
+    marginLeft: '110px',
+    marginTop: '100px',
+    marginBottom: '30px',
+  },
+
+  '.vertical-bar': {
+    height: '130px',
+    width: '0px',
+    border: '5px solid #EA9A28',
+    margin: 'auto',
+  },
 }
 
 const PageLanding = function () {
   return (
     <div sx={PageLandingSx}>
-      <div className="top-elements">
+      <div className="flex-elements centered">
         <div className="main-text">
           <h2 className="heading">Ace your next finance interview</h2>
           <div className="description centered">
@@ -77,11 +106,59 @@ const PageLanding = function () {
           <img src={meeting} alt="meeting" style={{ height: '200px' }} />
         </div>
       </div>
-      <div>
-        Submit Tps
+      <br />
+      <br />
+      <div className="flex-elements">
+        <div className="centered">
+          <img src={submitTPs} alt="Submit TPs" style={{ height: '300px' }} />
+        </div>
+        <div className="centered" style={{ marginRight: '200px' }}>
+          <h3>Submit Tps</h3>
+          <br />
+          <div className="body-text">
+            Users submit <em>thought processes</em> (TPs) to practice finance interview problems, broken down into your initial thoughts, different approaches, and final solution
+          </div>
+        </div>
       </div>
-      <div>
-        Post Feedback
+      <div className="flex-elements">
+        <div className="centered" style={{ marginLeft: '250px' }}>
+          <h3>Post Feedback</h3>
+          <br />
+          <div className="body-text">
+            Users provide feedback on TPs, fostering community discussion and improving the quality of answers.
+          </div>
+        </div>
+        <div className="centered">
+          <img src={postFeedbacks} alt="Post Feedback" style={{ height: '350px' }} />
+        </div>
+      </div>
+      <div className="flex-elements centered">
+        <div className="icon-container">
+          <div>
+            <img src={communicate} alt="Communicate" style={{ height: '140px' }} />
+          </div>
+          <div className="icon-caption">
+            Communicate your thought processes to solve complex questions
+          </div>
+        </div>
+        <div className="vertical-bar"></div>
+        <div className="icon-container">
+          <div>
+            <img src={lightbulb} alt="Learn" style={{ height: '140px' }} />
+          </div>
+          <div className="icon-caption">
+            Learn a variety of problem solving approaches and have your work evaluated
+          </div>
+        </div>
+        <div className="vertical-bar"></div>
+        <div className="icon-container">
+          <div>
+            <img src={network} alt="Community" style={{ height: '140px' }} />
+          </div>
+          <div className="icon-caption">
+            Join a collaborative community of aspiring financial professionals
+          </div>
+        </div>
       </div>
     </div>
   );
