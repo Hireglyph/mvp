696e5f9027cfb6890a31e14f9bccdea99f10ba73
Ashley Zhuang <ashleyzhuang01@gmail.com>
2021-05-17 20:30:55 -0700

    Clean up

--- src/utils/delete.js
+++ src/utils/delete.js
@@ -1,53 +1,44 @@
-export const tpDelete  = ({
-  firebase,
-  questId,
-  tpId,
-  uid,
-  }) => {
+export const tpDelete = ({ firebase, questId, tpId, uid }) => {
   const updates = {};
 
-  updates[`/tps/${questId}/${tpId}/initial`] = "[deleted]";
-  updates[`/tps/${questId}/${tpId}/approach`] = "[deleted]";
-  updates[`/tps/${questId}/${tpId}/solution`] = "[deleted]";
-  updates[`/tps/${questId}/${tpId}/username`] = "[deleted]";
+  updates[`/tps/${questId}/${tpId}/initial`] = '[deleted]';
+  updates[`/tps/${questId}/${tpId}/approach`] = '[deleted]';
+  updates[`/tps/${questId}/${tpId}/solution`] = '[deleted]';
+  updates[`/tps/${questId}/${tpId}/username`] = '[deleted]';
   updates[`/tps/${questId}/${tpId}/creator`] = null;
 
   updates[`tpHistory/${uid}/${tpId}`] = null;
 
-  firebase.update("/", updates);
+  firebase.update('/', updates);
 };
 
-export const feedbackDelete  = ({
-  firebase,
-  tpId,
-  feedbackId,
-  uid,
-  }) => {
+export const feedbackDelete = ({ firebase, tpId, feedbackId, uid }) => {
   const updates = {};
 
-  updates[`/feedbacks/${tpId}/${feedbackId}/feedback`] = "[deleted]";
-  updates[`/feedbacks/${tpId}/${feedbackId}/username`] = "[deleted]";
+  updates[`/feedbacks/${tpId}/${feedbackId}/feedback`] = '[deleted]';
+  updates[`/feedbacks/${tpId}/${feedbackId}/username`] = '[deleted]';
   updates[`/feedbacks/${tpId}/${feedbackId}/creator`] = null;
 
   updates[`feedbackHistory/${uid}/${feedbackId}`] = null;
 
-  firebase.update("/", updates);
+  firebase.update('/', updates);
 };
 
-export const replyDelete  = ({
+export const replyDelete = ({
   firebase,
   tpId,
   replyFeedbackID,
   replyId,
   uid,
-  }) => {
+}) => {
   const updates = {};
 
-  updates[`/replies/${tpId}/${replyFeedbackID}/${replyId}/reply`] = "[deleted]";
-  updates[`/replies/${tpId}/${replyFeedbackID}/${replyId}/username`] = "[deleted]";
+  updates[`/replies/${tpId}/${replyFeedbackID}/${replyId}/reply`] = '[deleted]';
+  updates[`/replies/${tpId}/${replyFeedbackID}/${replyId}/username`] =
+    '[deleted]';
   updates[`/replies/${tpId}/${replyFeedbackID}/${replyId}/creator`] = null;
 
   updates[`replyHistory/${uid}/${replyId}`] = null;
 
-  firebase.update("/", updates);
+  firebase.update('/', updates);
 };
